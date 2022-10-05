6f7c2a6b0e649fa1b2fcb0426a3fe6d545531254
TheS1lentKiller <gabrielcampos548@gmail.com>
2022-10-05 14:59:19 -0300

    correções

--- Atividade 1/questao23.dart
+++ Atividade 1/questao23.dart
@@ -7,11 +7,11 @@ void main(List<String> args) {
   int numero2 = int.parse(stdin.readLineSync()!);
 
   if (numero1 > numero2){
-    for (numero2; numero2 < numero1; numero2++){
+    for (numero2; numero2 <= numero1; numero2++){
       print(numero2);
     }
   }else if (numero2 > numero1){
-    for(numero1; numero1<numero2;numero1++){
+    for(numero1; numero1<=numero2;numero1++){
       print(numero1);
     }
   }
